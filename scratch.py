f94d8d33ad526932727ac3474532df722fecdde7
Haobo Chen <chenhaob@grinnell.edu>
2022-07-03 18:37:14 -0700

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,1 +1,12 @@
-print(1+1)
+from unicodedata import decimal
+
+
+def convert(octal):
+    decimal = 0
+    curr_place =0
+    while octal >0:
+        curr_digit = octal%10
+        decimal = curr_digit*curr_place^8 + decimal
+        curr_place = curr_place + 1
+        octal= octal //10
+    return decimal
