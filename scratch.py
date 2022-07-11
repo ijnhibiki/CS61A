4193f7376846bb696a3ef1892cd121d951bbd88c
Haobo Chen <chenhaob@grinnell.edu>
2022-07-11 12:10:29 -0700

    7.11

--- scratch.py
+++
@@ -1,7 +0,0 @@
-def clear_negatives(lst):
-    if len(lst) == 0:
-        return[]
-    elif lst[0] < 0:
-        return clear_negatives(lst[abs(list[0]):])
-    else:
-        return clear_negatives(list[1:])+ [lst[0]]
